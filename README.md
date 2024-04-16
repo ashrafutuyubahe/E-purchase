b34b1956759402972011deecfbcba92f03f56ce7
Ashrafu <ashraftuyubahe001@gmail.com>
2024-04-16 09:32:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# E-purchase
+this is an E-commerce application  that offer a range of product especially books 
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+    <h3>hello!!</h3>
+    <p>this is my first react applicationy</p>
+    </>
+  );
+}
+
+export default App;
