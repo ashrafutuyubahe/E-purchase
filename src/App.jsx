82a6b7f00a3b4d5951d112e936c8ed3428a05da0
Ashrafu <ashraftuyubahe001@gmail.com>
2024-04-19 07:24:28 +0200

    making data fetching api

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,34 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Cart from "./cart";
 
 function App() {
+      const [count,setCount]= useState(0)
+    const [productArray, setProductArray] = useState([
+      { product: "product 1" }, { product: "product 2" }
+    ]);
+  
+    function handleReset() {
+      setCount(0);
+    }
+  
+    const handleRemove = (productToRemove) => {
+      const updatedProductArray = productArray.filter(
+        (item) => item.product !== productToRemove
+      );
+      setProductArray(updatedProductArray);
+    };
+
   
   return (
    <Cart
-  //  count={count}
-  //  handleReset={handleReset}
-  //  productArray={productArray}
-  //  handleRemove={handleRemove}
+   
+   count={count} 
+   key={productArray.product}
+   setCount={setCount}
+   handleReset={handleReset}
+   productArray={productArray}
+   handleRemove={handleRemove}
+   setProductArray={setProductArray}
+
 
    />
   );
--- src/cart.js
+++ src/cart.js
@@ -1,58 +1,39 @@
-import React, { useState } from "react";
+import React from "react";
 
-    export default function Cart(){
+export default function Cart(props) {
 
-    const [count, setCount] = useState(0);
-    const [productArray, setProductArray] = useState([
-      { product: "product 1" }, { product: "product 2" }
-    ]);
-  
-    function handleReset() {
-      setCount(0);
-    }
-  
-    const handleRemove = (productToRemove) => {
-      const updatedProductArray = productArray.filter(
-        (item) => item.product !== productToRemove
-      );
-      setProductArray(updatedProductArray);
-    };
-      
 
-    return (
-        <>
-          <button className="m-5 btn btn-secondary">Cart: {count}</button>
-          <button className="btn btn-secondary" onClick={handleReset}>
-            Reset
+  return (
+    <>
+      <button className="m-5 btn btn-secondary">
+            Cart: {props.count}
+      </button>
+      <button className="btn btn-secondary" onClick={props.handleReset}>
+        Reset
+      </button>
+      {props.productArray.map((item,index) => (
+        <div className="d-flex m-4 justify-content-" ke660f0b31fab4e9f864a7bee2y={index}>
+          <input
+            type="text"
+            className="form-control w-25 bg-info"
+            value={item.product}
+            readOnly
+          />  
+          <button
+            className="m-2 btn btn-success"
+            onClick={() => {
+           props.setCount(props.count + 1);
+            }}
+          >
+            Add product
           </button>
-          {productArray.map((item) => (
-            <div className="d-flex m-4 justify-content-" key={item.product}>
-              <input
-                type="text"
-                className="form-control w-25 bg-info"
-                value={item.product}
-                readOnly
-              />
-              <button
-                className="m-2 btn btn-success"
-                onClick={() => {
-                  setCount(count + 1);
-                }}
-              >
-                Add product
-              </button>
-              <button
-                className="btn btn-warning"
-                onClick={() => handleRemove(item.product)}
-              >
-                Remove product
-              </button>
-            </div>
-          ))}
-        </>
-      );
-
-} 
-
-
-
+          <button className="btn btn-warning"  onClick={()=>{props.handleRemove(item.product)}}>
+            Remove product
+          </button>
+        </div>   
+        
+    
+      ))}  
+    </>
+  );           
+}
