90f6dbda2710b46e85d7765bcc7973b2f3c1700a
Ashrafu <ashraftuyubahe001@gmail.com>
2024-04-20 08:28:08 +0200

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,46 +3,45 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Cart from "./cart";
 import Nav from "./navbar";
 import Fetchdata from "./datafetching";
+import axios from "axios";
 
+axios.defaults.baseURL = "http://127.0.0.1:6000";
+axios.defaults.withCredentials = true;
 
 function App() {
-      const [count,setCount]= useState(0)
-    const [productArray, setProductArray] = useState([
-      { product: "product 1" }, { product: "product 2" }
-    ]);
-  
-    function handleReset() {
-      setCount(0);
-    }
-  
-    const handleRemove = (productToRemove) => {
-      const updatedProductArray = productArray.filter(
-        (item) => item.product !== productToRemove
-      );
-      setProductArray(updatedProductArray);
-    };
+  const [count, setCount] = useState(0);
+  const [productArray, setProductArray] = useState([
+    { product: "product 1" },
+    { product: "product 2" },
+  ]);
 
-  
+  function handleReset() {
+    setCount(0);
+  }
+
+  const handleRemove = (productToRemove) => {
+    const updatedProductArray = productArray.filter(
+      (item) => item.product !== productToRemove
+    );
+    setProductArray(updatedProductArray);
+  };
+
   return (
+    <>
+      <Nav />
 
-   <>
-   <Nav/>
-    
-    <Cart
-   
-   count={count} 
-   key={productArray.product}
-   setCount={setCount}
-   handleReset={handleReset}
-   productArray={productArray}
-   handleRemove={handleRemove}
-   setProductArray={setProductArray}
-   />
-   
-    
-   <Fetchdata/>
+      <Cart
+        count={count}
+        key={productArray.product}
+        setCount={setCount}
+        handleReset={handleReset}
+        productArray={productArray}
+        handleRemove={handleRemove}
+        setProductArray={setProductArray}
+      />
 
-   </>
+      <Fetchdata />
+    </>
   );
 }
 
--- src/datafetching.js
+++ src/datafetching.js
@@ -1,32 +1,52 @@
-
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Fetchdata() {
-  const [blogs, setblogs] = useState(null);
+  const [blogs, setBlogs] = useState(null);
+
+  //data fetching using Axios library
 
   useEffect(() => {
-    console.log("hello this is useEffect");
+    axios
+      .get("http://localhost:7600/blogs")
+      .then((response) => {
+        console.log(response.data);
+        setBlogs(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    fetch("http://localhost:6000//blogs")
-      .then((res) => {
-  return res.json
-      })
-    .then((data)=>{
-        console.log(data);
-    })
-     .catch((error)=>{
-   console.log(error)
-      })
-  });
+  //data fetching using fetching API
+  //   useEffect(() => {
+  //     console.log("hello this is useEffect");
+
+  //     fetch("http://localhost:7600/blogs")
+  //       .then((res) => res.json())
+  //                .then((data) => {
+  //console.log(data);
+  //          setBlogs(data);
+  //       })
+  //       .catch((error) => {
+  //         console.log(error);
+  //       });
+  //   }, []);
 
   return (
     <>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium
-        impedit magnam, atque nostrum omnis molestiae labore illum ea sequi
-        fugit, debitis illo exercitationem, quia fuga obcaecati architecto
-        praesentium sed sapiente!
-      </p>
+    <div className="container flex m-2">
+    {blogs &&
+        blogs.map((blog) => (
+          <div key={blog._id} className=" m-2 bg-info" >
+            <h2>{blog.data.blogtitle}</h2>
+            <p>{blog.data.blogdescription}</p>
+            <p>{blog.data.authorname}</p>
+            <p>{blog.data.date}</p>
+          </div>
+        ))}
+    </div>
+    
     </>
   );
 }
