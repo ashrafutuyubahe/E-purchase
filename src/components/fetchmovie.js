6a38cc4d06302892aca2ebfd00ee4cd148fc6060
Ashrafu <ashraftuyubahe001@gmail.com>
2024-04-24 10:43:19 +0200

    completed fetching of movie

--- src/components/fetchmovie.js
+++ src/components/fetchmovie.js
@@ -1,11 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function FetchMovie() {
   const apiUrl =
     "https://api.themoviedb.org/3/discover/movie?api_key=38868210c902bc6cad8040a281134b8b";
-  const api_key = "38868210c902bc6cad8040a281134b8b";
 
-  const [movies,getMovies] = useEffect([]);
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     fetch(apiUrl)
@@ -13,12 +12,11 @@ export default function FetchMovie() {
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
-
         return response.json();
       })
       .then((data) => {
-        console.log("Response from Movia API:", data.results);
-        getMovies(data.results);
+        console.log("Response from Movie API:", data.results);
+        setMovies(data.results);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
@@ -26,13 +24,21 @@ export default function FetchMovie() {
   }, []);
 
   return (
-    <>
-      {/* <div>
-      {console.log(movies)}
-        {movies.map((movie) => {
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}}`} />;
-        })}
-      </div> */}
-    </>
+    <div>
+      {movies &&
+        movies.map((movie) => (
+         <a  href="#">
+           <img
+            key={movie.id}
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.title}
+             style={{width:"300px",height:"250px", marginLeft:"10px",marginTop:"10px"}}
+          
+          />
+         
+        
+         </a>
+        ))}
+    </div>
   );
 }
